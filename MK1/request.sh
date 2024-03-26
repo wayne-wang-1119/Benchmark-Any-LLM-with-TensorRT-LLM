cb006831328449925de387574fde382f138c56d8
wayne-wang-1119 <haohanw@eecs.berkeley.edu>
2024-03-26 10:35:45 -0700

    update benchmark.py and request.sh

--- MK1/request.sh
+++ MK1/request.sh
@@ -1,8 +1,1 @@
-curl -X "POST" "https://mk1--mk1-chat-endpoint-dev.modal.run/generate" -H 'Content-Type: application/json' -d '{
-  "text": "What is the difference between a llama and an alpaca?",
-  "max_tokens": 512,
-  "eos_token_ids": [1, 2],
-  "temperature": 0.8,
-  "top_k": 50,
-  "top_p": 1.0
-}'
+python gpt_query_experiment.py
